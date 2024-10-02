d203e60f18e6f27326eb7a708e302f5392983f2c
Will Sharpe <wsharpe@sipsdev.sips>
2024-10-02 13:48:41 +0000

    fixed aerdb readers

--- satpy_viirs_l2/viirs_l2.py
+++ satpy_viirs_l2/viirs_l2.py
@@ -170,20 +170,25 @@ class VIIRSL2FileHandler(NetCDF4FileHandler):
         # FIXME
         # For aerdb Longitude and Latitude datasets have coordinates
         # This check is needed to work with yaml_reader
-        if "long_name" in metadata and metadata["long_name"] == "Longitude":
-            data.coords["Latitude"].attrs["standard_name"] = "latitude"
-        elif "long_name" in metadata and metadata["long_name"] == "Latitude":
-            data.coords["Longitude"].attrs["standard_name"] = "longitude"
-
-        data.attrs.update(metadata)
+        if 'aerdb_l2_viirs' in ds_info['file_type']:
+            data=data.drop_indexes(coord_names=['Idx_Atrack','Idx_Xtrack']).reset_coords(drop=True)
         if valid_min is not None and valid_max is not None:
             data = data.where((data >= valid_min) & (data <= valid_max))
+            if not metadata.get('valid_min'):
+                metadata['valid_min']=valid_min
+            if not metadata.get('valid_max'):
+                metadata['valid_max']=valid_max
         factors = (scale_factor, scale_offset)
         factors = self.adjust_scaling_factors(factors, metadata["file_units"], ds_info.get("units"))
         if factors[0] != 1 or factors[1] != 0:
             data *= factors[0]
             data += factors[1]
         # rename dimensions to correspond to satpy's 'y' and 'x' standard
+        data.attrs.update(metadata)
         if "number_of_lines" in data.dims:
             data = data.rename({"number_of_lines": "y", "number_of_pixels": "x"})
+        elif "number_of_lines_8x8" in data.dims:
+            data = data.rename({"number_of_lines_8x8": "y", "number_of_pixels_8x8": "x"})
+        elif "Idx_Atrack" in data.dims:
+            data = data.rename({"Idx_Atrack": "y", "Idx_Xtrack": "x"})
         return data
